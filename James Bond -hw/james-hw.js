1b821ef28612be383e2b5b7b613c43838cd20724
julijajarmale <julija.jarmale@gmail.com>
2022-05-17 15:41:10 +0300

    po paskaitos

--- James Bond -hw/james-hw.js
+++ James Bond -hw/james-hw.js
@@ -1,17 +1,57 @@
+//EVENT
+//1. Susirandame taga <>
+//2. Susirandam ivyki
+//3. Pradedame stebeti// vienu metu galima stebeti vieno elemento viena ivyki;
+console.log(`1 UZDAVINYS`)
+
 //1.Atskiri elementai
 //A)Tamsiai žaliai nuspalvinti h1 tagą;
+const h1 = document.querySelector('h1');
+h1.style.color = 'green'
+
+console.log(h1)
+
 //B)Tagui i pridėti klasę small;
 //https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+const i = document.querySelector('i');
+i.classList.add("small");
+console.log(i);
+
 //C)Iš tago h1 pašalinti klasę main;
+h1.classList.remove("main");
+
 //D)Tik tam h2 tagui, kuris iškart eina po h1 tago, pridėti klasę first ir pašalinti klasę main;
 //https://developer.mozilla.org/en-US/docs/Web/API/DOMTokenList/remove
+const h2First = document.querySelector('h2')
+h2First.classList.add("first");
+h2First.classList.remove("main");
+
 //E)Pirmam span tagui, kuris yra h2 viduje sumažinti fonto dydį iki 10px ir nudažyti pilkai
-console.log(`1 UZDAVINYS`)
+console.log(`2 UZDAVINYS`)
+const firstSpan = document.querySelector('h2 > span');
+firstSpan.classList.add("span");
 
 //2.Elemetų grupės (nodeList) skaičiavimus išvest į consolę
 //A)Suskaičiuoti kiek yra h2 tagų;
+
+const h2All = document.querySelectorAll('h2');
+console.log(`h2 tagų yra ${h2All.length}`)
+
 //B)Suskaičiuoti kiek yra h2 tagų, kurie neturi klasės first
+
+let h2NoFirst = 0;
+for (let i = 0; i< h2All.length; i++){
+  if (!h2All[i].classList.contains('first')){
+    h2NoFirst++
+  } 
+
+}
+console.log(`Firs klases neturi ${h2NoFirst} h2 tagai`)
+
+
 //C)Visus h2 tagus nuspalvinti šviesiai mėlynai
+h2All.style.color = 'lightblue'
+
 //D)Tagų, kurie yra div su klase prices, viduje esantiems h2 tagams pridėti klasę price-tag;
 //E)Pabraukti visus tagus su klase new;
 //F)Suskaičiuoti kiek yra gyvūnų kategorijų ir žirafų (jos yra ul tagai);
