eed04e593ea0c1bd5a90f3be21f2614320b92394
julijajarmale <julija.jarmale@gmail.com>
2022-05-23 11:02:51 +0300

    3

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,21 +1,8 @@
 
 import './App.css';
 import Bala from './Components/HomeworkCoponents/Bala';
-import Daiktas from './Components/HomeworkCoponents/Daiktas';
+import Jura from './Components/HomeworkCoponents/Jura';
 import Tvenkinys from './Components/HomeworkCoponents/Tvenkinys';
-const seaPlaners = [
-  {id: 1, type: 'man', name: 'Lina', color: 'blue'},
-  {id: 2, type: 'car', name: 'Opel', color: 'red'},
-  {id: 3, type: 'animal', name: 'Vilkas', color: 'green'},
-  {id: 4, type: 'fish', name: 'Ungurys', color: 'yellow'},
-  {id: 5, type: 'man', name: 'Tomas', color: 'green'},
-  {id: 6, type: 'animal', name: 'Bebras', color: 'red'},
-  {id: 7, type: 'animal', name: 'Barsukas', color: 'green'},
-  {id: 8, type: 'car', name: 'MB', color: 'blue'},
-  {id: 9, type: 'car', name: 'ZIL', color: 'red'},
-  {id: 10, type: 'man', name: 'Teta Toma', color: 'yellow'},
-];
-
 
 
 
@@ -26,6 +13,7 @@ function App() {
       <header className="App-header">
       <Bala></Bala>
       <Tvenkinys></Tvenkinys>
+      <Jura></Jura>
       </header>
     </div>
   );
