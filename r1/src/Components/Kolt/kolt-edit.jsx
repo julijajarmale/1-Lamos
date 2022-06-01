1cf716bdf3ec6adb6d1432625b0241c7630034e0
julijajarmale <julija.jarmale@gmail.com>
2022-06-01 22:07:09 +0300

    saugo!

--- r1/src/Components/Kolt/kolt-edit.jsx
+++ r1/src/Components/Kolt/kolt-edit.jsx
@@ -29,19 +29,19 @@ function KoltEdit({modalData, setModalData, setEditData}) {
         <div className="modal">
         <div className='form'>
         <div>
-          <h2>Naujo paspirtuko forma</h2>  
+          <h2>Atnaujinti paspirtuko informaciją</h2>  
         </div>
         <div className="form-group">
-            <label className="label" onChange={e => setCode(e.target.value)} value={code}>Registration Code:</label>
-            <input type="text" className="form-row"/>
+            <label className="label">Registration Code:</label>
+            <input type="text" className="form-row" onChange={e => setCode(e.target.value)} value={code}/>
         </div>
         <div className="form-group">
-            <label className="label" onChange={e => setTime(e.target.value)} value={time}>Last time used:</label>
-            <input type="text" className="form-row"/>
+            <label className="label">Last time used:</label>
+            <input type="text" className="form-row"  onChange={e => setTime(e.target.value)} value={time}/>
         </div>
         <div className="form-group">
-            <label className="label" onChange={e => setKm(e.target.value)} value={km}>Total Kilometers Ride:</label>
-            <input type="text" className="form-row"/>
+            <label className="label">Total Kilometers Ride:</label>
+            <input type="text" className="form-row" onChange={e => setKm(e.target.value)} value={km}/>
         </div>
         <button type="button" className="btn" onClick={() => setModalData(null)}>Uždaryti</button>
         <button type="button" className="btn" onClick={koltEdit}>Išsaugoti</button>
