9d11a0860c6acf8d1dd13a3cde29c28406313a4b
julijajarmale <julija.jarmale@gmail.com>
2022-06-06 12:22:39 +0300

    grybas

--- 008/008.js
+++ 008/008.js
@@ -1,3 +1,5 @@
+
+
 class Stikline {
 
     static gerimukas = 'Pepsi';
@@ -59,3 +61,39 @@ Stikline.registras[0].ipilti(40);
 s1.stiklinejeYra();
 s2.stiklinejeYra();
 s3.stiklinejeYra();
+
+
+class Grybas{
+
+    constructor() {
+        this.valgomas = !this.rand(0, 1);
+        this.sukirmijes = !this.rand(0, 1);
+        this.svoris = this.rand(5, 45);
+
+    }
+     rand(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min)
+      }
+}
+
+class Krepsys{
+
+    constructor(){
+        this.dydis = 500;
+        this.prikrauta = 0;
+    }
+    deti(grybas){
+        if(grybas.valgomas && !grybas.sukirmijes){
+            this.prikrauta +=grybas.svoris;
+        }
+        return this.prikrauta < this.dydis;
+    }
+}
+
+const Kr = new Krepsys();
+
+while(Kr.deti(new Grybas())){}
+
+console.log(Kr);
