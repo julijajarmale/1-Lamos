8ea8f017fcf7e8b2c03da55e57e7cb1b1020bdb0
julijajarmale <julija.jarmale@gmail.com>
2022-06-15 14:28:15 +0300

    po pask

---
+++ r1/src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useReducer } from 'react';
+import './App.scss';
+import ld from './Reducers/ld';
+
+const masyvas = [
+    {id:3, name: 'Peter', bid: 487.77, date: 'December 17, 2022 03:42:00'},
+    {id:7, name: 'Mary', bid: 125.33, date: 'March 17, 2022 03:42:00'},
+    {id:8, name: 'Ąžuolas', bid: 78.25, date: '3/1/22'},
+    {id:9, name: 'Petras Dainorius', bid: 487.77, date: '2022-06-01T08:13'}
+];
+
+function App() {
+
+const [list, dispachList] = useReducer(ld, masyvas) // ld - dispatcheris, masyvas - defaultas
+const [select, setSelect] = useState('bid_desc')
+
+useEffect(() => {  //useefect veikia tada kai pasikeicia selectras
+dispachList({type: select})
+
+},[select])
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+              <h1>Sort reducer</h1>
+              <dvi className="kvc">
+                <div>
+                    <select value={select} onChange={e => setSelect(e.target.value)}>
+                        <option value="date_asc">DATE ASC</option>
+                        <option value="date_desc">DATE DESC</option>
+                        <option value="bid_asc">BID ASC</option>
+                        <option value="bid_desc">BID DESC</option>
+                        <option value="name_asc">NAME ASC</option>
+                        <option value="name_desc">NAME DESC</option>
+                        <option value="name_asc_local">NAME ASC LOC</option>
+                        <option value="random">RAND</option>
+                    </select>
+                </div>
+                <div>
+                    {
+                        list.map(b => 
+                        <div>
+                            <span>ID: {b.id}</span>
+                            <span>NAME: {b.name}</span>
+                            <span>BID: {b.bid}</span>
+                            <span>DATE: {b.date}</span>
+                        </div>)
+                    }
+
+                </div>
+              </dvi>
+            </header>
+        </div>
+    );
+
+    
+}
+
+export default App;
---
+++ r1/src/Reducers/ld.js
@@ -0,0 +1,69 @@
+import rand from "../Functions/rand";
+
+function ld(state, action) {
+    const newState = [...state];  //is kart pasidarom kopija kad galetume modifikuot
+
+    switch(action.type) {
+        case 'bid_desc':
+            newState.sort((a, b) => b.bid - a.bid);
+            break;
+            
+        case 'bid_asc':
+            newState.sort((a, b) => a.bid - b.bid);
+            break;
+            
+        case 'name_asc':
+            newState.sort((a, b) => {
+            if (a.name > b.name) return 1;
+            if (a.name < b.name) return -1;
+            return 0;
+
+            });
+            break;
+
+        case 'name_desc':
+        newState.sort((a, b) => {
+        if (a.name > b.name) return -1;
+        if (a.name < b.name) return 1;
+        return 0;
+        });
+        break;
+
+        case 'name_asc_local':
+            newState.sort((a, b) => a.name.localeCompare(b.name, 'lt'));
+            break;
+
+    
+
+         case 'date_desc':
+             newState.sort((a, b) => {
+                const ad = new Date(a.date);
+                const bd = new Date(b.date);
+
+             if (ad > bd) return -1;
+             if (ad < bd) return 1;
+             return 0;
+             });
+             break;
+
+        case 'date_asc':
+           newState.sort((a, b) => {
+              const ad = new Date(a.date);
+              const bd = new Date(b.date);
+           if (ad > bd) return 1;
+           if (ad < bd) return -1;
+           return 0;
+           });
+           break;
+
+            case 'random':
+           newState.sort((a, b) => rand(0,1) ? 1: -1);
+           break;
+            default:
+    }
+
+    return newState
+}
+
+export default ld;
+
