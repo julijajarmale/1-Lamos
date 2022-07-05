69d4493b66ef9308deaa47665d0cfda9f4f6f329
julijajarmale <julija.jarmale@gmail.com>
2022-07-05 11:58:43 +0300

    comments done

--- server/app.js
+++ server/app.js
@@ -19,193 +19,197 @@ const con = mysql.createConnection({
     database: "lama",
 });
 
-////Routes
-////READ
-//// SELECT column_name(s)
-//// FROM table1
-//// LEFT JOIN table2
-//// ON table1.column_name = table2.column_name;
-//app.get("/medziai", (req, res) => {
-//    const sql = `
-//    SELECT
-//    t.title, g.title AS good, height, type, t.id, GROUP_CONCAT(c.com, '-^o^-') AS coms, GROUP_CONCAT(c.id) AS coms_id
-//    FROM trees AS t
-//    LEFT JOIN goods AS g
-//    ON t.goods_id = g.id
-//    LEFT JOIN comments AS c
-//    ON c.tree_id = t.id
-//    GROUP BY t.id
-//`;
-//    con.query(sql, (err, result) => {
-//        if (err) throw err;
-//        res.send(result);
-//    });
-//});
-//app.get("/gerybes", (req, res) => {
-//    const sql = `
-//  SELECT
-//  g.title, g.id, COUNT(t.id) AS trees_count
-//  FROM trees AS t
-//  RIGHT JOIN goods AS g
-//  ON t.goods_id = g.id
-//  GROUP BY g.id
-//  ORDER BY trees_count DESC
-//`;
-//    con.query(sql, (err, result) => {
-//        if (err) throw err;
-//        res.send(result);
-//    });
-//});
-//
-//
-//app.get("/front/gerybes", (req, res) => {
-//    const sql = `
-//  SELECT
-//  g.title, g.id, COUNT(t.id) AS trees_count, GROUP_CONCAT(t.title) as tree_titles
-//  FROM trees AS t
-//  RIGHT JOIN goods AS g
-//  ON t.goods_id = g.id
-//  GROUP BY g.id
-//  ORDER BY g.title
-//`;
-//    con.query(sql, (err, result) => {
-//        if (err) throw err;
-//        res.send(result);
-//    });
-//});
-//
-//app.get("/front/medziai", (req, res) => {
-//    const sql = `
-//  SELECT
-//  t.title, g.title AS good, height, type, t.id, GROUP_CONCAT(c.com, '-^o^-') AS coms, t.rates, t.rate_sum
-//  FROM trees AS t
-//  LEFT JOIN goods AS g
-//  ON t.goods_id = g.id
-//  LEFT JOIN comments AS c
-//  ON c.tree_id = t.id
-//  GROUP BY t.id
-//`;
-//    con.query(sql, (err, result) => {
-//        if (err) throw err;
-//        res.send(result);
-//    });
-//});
-//
-////CREATE
-//
-//// INSERT INTO table_name (column1, column2, column3, ...)
-//// VALUES (value1, value2, value3, ...);
-//app.post("/medziai", (req, res) => {
-//    const sql = `
-//INSERT INTO trees
-//(type, title, height, goods_id)
-//VALUES (?, ?, ?, ?)
-//`;
-//    con.query(sql, [req.body.type, req.body.title, req.body.height ? req.body.height : 0, req.body.good !== '0' ? req.body.good : null], (err, result) => {
-//        if (err) throw err;
-//        res.send({ result, msg: { text: 'OK, Zuiki', type: 'success' } });
-//    });
-//});
-//app.post("/gerybes", (req, res) => {
-//    const sql = `
-//INSERT INTO goods
-//(title)
-//VALUES (?)
-//`;
-//    con.query(sql, [req.body.title], (err, result) => {
-//        if (err) throw err;
-//        res.send({ result, msg: { text: 'OK, Zuiki', type: 'success' } });
-//    });
-//});
-//
-//app.post("/front/komentarai", (req, res) => {
-//    const sql = `
-//INSERT INTO comments
-//(com, tree_id)
-//VALUES (?, ?)
-//`;
-//    con.query(sql, [req.body.com, req.body.treeId], (err, result) => {
-//        if (err) throw err;
-//        res.send({ result, msg: { text: 'OK, Zuiki', type: 'success' } });
-//    });
-//});
-//
-//
-////DELETE
-//// DELETE FROM table_name WHERE condition;
-//app.delete("/medziai/:treeId", (req, res) => {
-//    const sql = `
-//DELETE FROM trees
-//WHERE id = ?
-//`;
-//    con.query(sql, [req.params.treeId], (err, result) => {
-//        if (err) throw err;
-//        res.send({ result, msg: { text: 'OK, Bebrai', type: 'info' } });
-//    });
-//});
-//
-//app.delete("/gerybes/:goodId", (req, res) => {
-//    const sql = `
-//DELETE FROM goods
-//WHERE id = ?
-//`;
-//    con.query(sql, [req.params.goodId], (err, result) => {
-//        if (err) throw err;
-//        res.send({ result, msg: { text: 'OK, Bebrai', type: 'info' } });
-//    });
-//});
-//
-//app.delete("/komentarai/:comId", (req, res) => {
-//    const sql = `
-//DELETE FROM comments
-//WHERE id = ?
-//`;
-//    con.query(sql, [req.params.comId], (err, result) => {
-//        if (err) throw err;
-//        res.send({ result, msg: { text: 'Komentaro pabaiga', type: 'info' } });
-//    });
-//});
-////EDIT
-//// UPDATE table_name
-//// SET column1 = value1, column2 = value2, ...
-//// WHERE condition;
-//app.put("/medziai/:treeId", (req, res) => {
-//    const sql = `
-//    UPDATE trees
-//    SET title = ?, type = ?, height = ?, good_id = ?
-//    WHERE id = ?
-//`;
-//    con.query(sql, [req.body.title, req.body.type, req.body.height, req.body.good, req.params.treeId], (err, result) => {
-//        if (err) throw err;
-//        res.send({ result, msg: { text: 'OK, Barsukai', type: 'danger' } });
-//    });
-//});
-//
-//
-//app.put("/front/balsuok/:treeId", (req, res) => {
-//    const sql = `
-//    UPDATE trees
-//    SET rates = rates + 1, rate_sum = rate_sum + ?
-//    WHERE id = ?
-//`;
-//    con.query(sql, [req.body.rate, req.params.treeId], (err, result) => {
-//        if (err) throw err;
-//        res.send({ result, msg: { text: 'Tu prabalsavai', type: 'danger' } });
-//    });
-//});
-//app.listen(port, () => {
-//    console.log(`Bebras klauso porto Nr ${port}`);
-//});
-//
-//////*****************************KOLT****************************************************
-//
+//////Routes
+//////READ
+////// SELECT column_name(s)
+////// FROM table1
+////// LEFT JOIN table2
+////// ON table1.column_name = table2.column_name;
+////app.get("/medziai", (req, res) => {
+////    const sql = `
+////    SELECT
+////    t.title, g.title AS good, height, type, t.id, GROUP_CONCAT(c.com, '-^o^-') AS coms, GROUP_CONCAT(c.id) AS coms_id
+////    FROM trees AS t
+////    LEFT JOIN goods AS g
+////    ON t.goods_id = g.id
+////    LEFT JOIN comments AS c
+////    ON c.tree_id = t.id
+////    GROUP BY t.id
+////`;
+////    con.query(sql, (err, result) => {
+////        if (err) throw err;
+////        res.send(result);
+////    });
+////});
+////app.get("/gerybes", (req, res) => {
+////    const sql = `
+////  SELECT
+////  g.title, g.id, COUNT(t.id) AS trees_count
+////  FROM trees AS t
+////  RIGHT JOIN goods AS g
+////  ON t.goods_id = g.id
+////  GROUP BY g.id
+////  ORDER BY trees_count DESC
+////`;
+////    con.query(sql, (err, result) => {
+////        if (err) throw err;
+////        res.send(result);
+////    });
+////});
+////
+////
+////app.get("/front/gerybes", (req, res) => {
+////    const sql = `
+////  SELECT
+////  g.title, g.id, COUNT(t.id) AS trees_count, GROUP_CONCAT(t.title) as tree_titles
+////  FROM trees AS t
+////  RIGHT JOIN goods AS g
+////  ON t.goods_id = g.id
+////  GROUP BY g.id
+////  ORDER BY g.title
+////`;
+////    con.query(sql, (err, result) => {
+////        if (err) throw err;
+////        res.send(result);
+////    });
+////});
+////
+////app.get("/front/medziai", (req, res) => {
+////    const sql = `
+////  SELECT
+////  t.title, g.title AS good, height, type, t.id, GROUP_CONCAT(c.com, '-^o^-') AS coms, t.rates, t.rate_sum
+////  FROM trees AS t
+////  LEFT JOIN goods AS g
+////  ON t.goods_id = g.id
+////  LEFT JOIN comments AS c
+////  ON c.tree_id = t.id
+////  GROUP BY t.id
+////`;
+////    con.query(sql, (err, result) => {
+////        if (err) throw err;
+////        res.send(result);
+////    });
+////});
+////
+//////CREATE
+////
+////// INSERT INTO table_name (column1, column2, column3, ...)
+////// VALUES (value1, value2, value3, ...);
+////app.post("/medziai", (req, res) => {
+////    const sql = `
+////INSERT INTO trees
+////(type, title, height, goods_id)
+////VALUES (?, ?, ?, ?)
+////`;
+////    con.query(sql, [req.body.type, req.body.title, req.body.height ? req.body.height : 0, req.body.good !== '0' ? req.body.good : null], (err, result) => {
+////        if (err) throw err;
+////        res.send({ result, msg: { text: 'OK, Zuiki', type: 'success' } });
+////    });
+////});
+////app.post("/gerybes", (req, res) => {
+////    const sql = `
+////INSERT INTO goods
+////(title)
+////VALUES (?)
+////`;
+////    con.query(sql, [req.body.title], (err, result) => {
+////        if (err) throw err;
+////        res.send({ result, msg: { text: 'OK, Zuiki', type: 'success' } });
+////    });
+////});
+////
+////app.post("/front/komentarai", (req, res) => {
+////    const sql = `
+////INSERT INTO comments
+////(com, tree_id)
+////VALUES (?, ?)
+////`;
+////    con.query(sql, [req.body.com, req.body.treeId], (err, result) => {
+////        if (err) throw err;
+////        res.send({ result, msg: { text: 'OK, Zuiki', type: 'success' } });
+////    });
+////});
+////
+////
+//////DELETE
+////// DELETE FROM table_name WHERE condition;
+////app.delete("/medziai/:treeId", (req, res) => {
+////    const sql = `
+////DELETE FROM trees
+////WHERE id = ?
+////`;
+////    con.query(sql, [req.params.treeId], (err, result) => {
+////        if (err) throw err;
+////        res.send({ result, msg: { text: 'OK, Bebrai', type: 'info' } });
+////    });
+////});
+////
+////app.delete("/gerybes/:goodId", (req, res) => {
+////    const sql = `
+////DELETE FROM goods
+////WHERE id = ?
+////`;
+////    con.query(sql, [req.params.goodId], (err, result) => {
+////        if (err) throw err;
+////        res.send({ result, msg: { text: 'OK, Bebrai', type: 'info' } });
+////    });
+////});
+////
+////app.delete("/komentarai/:comId", (req, res) => {
+////    const sql = `
+////DELETE FROM comments
+////WHERE id = ?
+////`;
+////    con.query(sql, [req.params.comId], (err, result) => {
+////        if (err) throw err;
+////        res.send({ result, msg: { text: 'Komentaro pabaiga', type: 'info' } });
+////    });
+////});
+//////EDIT
+////// UPDATE table_name
+////// SET column1 = value1, column2 = value2, ...
+////// WHERE condition;
+////app.put("/medziai/:treeId", (req, res) => {
+////    const sql = `
+////    UPDATE trees
+////    SET title = ?, type = ?, height = ?, good_id = ?
+////    WHERE id = ?
+////`;
+////    con.query(sql, [req.body.title, req.body.type, req.body.height, req.body.good, req.params.treeId], (err, result) => {
+////        if (err) throw err;
+////        res.send({ result, msg: { text: 'OK, Barsukai', type: 'danger' } });
+////    });
+////});
+////
+////
+////app.put("/front/balsuok/:treeId", (req, res) => {
+////    const sql = `
+////    UPDATE trees
+////    SET rates = rates + 1, rate_sum = rate_sum + ?
+////    WHERE id = ?
+////`;
+////    con.query(sql, [req.body.rate, req.params.treeId], (err, result) => {
+////        if (err) throw err;
+////        res.send({ result, msg: { text: 'Tu prabalsavai', type: 'danger' } });
+////    });
+////});
+////app.listen(port, () => {
+////    console.log(`Bebras klauso porto Nr ${port}`);
+////});
+////
+////////*****************************KOLT****************************************************
+////
 //Routes
 //READ
 app.get("/paspirtukai", (req, res) => {
     const sql = `
-  SELECT
-  *
-  FROM kolt
+    SELECT
+    k.code, time, km, isbusy, k.id, GROUP_CONCAT(c.com, '-^o^-') AS coms, GROUP_CONCAT(c.id) AS coms_id
+    FROM kolt AS k
+    LEFT JOIN kolt_comments AS c
+    ON c.kolt_id = k.id
+    GROUP BY k.id
+ 
 `;
     con.query(sql, (err, result) => {
         if (err) throw err;
@@ -213,15 +217,18 @@ app.get("/paspirtukai", (req, res) => {
     });
 });
 
+
+
+
 app.get("/front/paspirtukai", (req, res) => {
     const sql = `
 
       SELECT
-      kolt.code, time, km, kolt.id, GROUP_CONCAT(c.com, '-^o^-') AS coms
-      FROM kolt 
+      k.code, time, km, isbusy, k.id, GROUP_CONCAT(c.com, '-^o^-') AS coms, GROUP_CONCAT(c.id) AS coms_id
+      FROM kolt AS k
       LEFT JOIN kolt_comments AS c
-      ON c.kolt_id = kolt.id
-      GROUP BY kolt.id
+      ON c.kolt_id = k.id
+      GROUP BY k.id
     `;
 
     con.query(sql, (err, result) => {
@@ -247,8 +254,8 @@ VALUES (?, ?, ?)
 
 app.post("/front/komentarai", (req, res) => {
     const sql = `
-INSERT INTO comments
-(com, kolt_Id)
+INSERT INTO kolt_comments
+(com, kolt_id)
 VALUES (?, ?)
 `;
     con.query(sql, [req.body.com, req.body.koltId], (err, result) => {
